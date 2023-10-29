ef068b6adda3743be79fc937de56f88d3cdd2c16
littyann <littyannjose14@gmail.com>
2023-10-29 19:40:29 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color,setColor] = useState("black")
+
+  const click =(color)=>{
+    setColor(color)
+
+  }
+
+  useEffect(()=>{
+    document.body.style.backgroundColor = color
+  },[color])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < >
+      
+      <div className='container'>
+        <h1>Background Color Change</h1>
+
+    <div className='button'>
+    <button onClick={()=>click("green")} className='btn btn-success p-2 mt-2' >Green</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <button onClick={()=>click("red")} className='btn btn-danger p-2 mt-2' >Red</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  
+         <button onClick={()=>click("blue")} className='btn btn-info p-2 mt-2'>Blue</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+         <button onClick={()=>click("black")} className='btn btn-dark p-2 mt-2'>Black</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+         <button onClick={()=>click("white")} className='btn btn-light p-2 mt-2'>White</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        
+         <button onClick={()=>click("pink")} className='btn btn-secondary p-2 mt-2'>Pink</button>
     </div>
+  
+      </div>  
+    </>
   );
 }
 
